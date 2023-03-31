368c0e0733316b3717e69c094234c022eabb7225
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-03-31 20:14:41 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,15 +7,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handleRequest() {
+func main() {
 	router := mux.NewRouter()
+
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my API!")
 	})
 
 	http.ListenAndServe(":8080", router)
-}
-
-func main() {
-	handleRequest()
 }
