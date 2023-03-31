251213a81079dc867e0a618a9bbf02d38377908d
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-03-31 10:40:25 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,33 @@
-provider "aws" {
-  region = "us-west-2"
+################################
+# Create Storage Bucket - Main #
+################################
+
+# Create a GCS Bucket
+resource "google_storage_bucket" "tf-bucket" {
+  project       = var.gcp_project
+  name          = var.bucket-name
+  location      = var.gcp_region
+  force_destroy = true
+  storage_class = var.storage-class
+  versioning {
+    enabled = true
+  }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket"
-  acl    = "private"
+# Create a Simple VM
+ resource "google_compute_instance" "default" {
+  name         = var.vm-name
+  machine_type = var.machine_type
+  zone         = var.gcp_zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
