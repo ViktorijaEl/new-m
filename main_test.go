208d5e9319b14e7bf7002835055972847018de1c
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-03-31 20:10:54 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
-
-	"github.com/gorilla/mux"
+    "net/http"
+    "net/http/httptest"
+    "testing"
 )
 
 func TestHandleRequest(t *testing.T) {
-	req, err := http.NewRequest("GET", "/hello", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+    req, err := http.NewRequest("GET", "/", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	rr := httptest.NewRecorder()
-	router := mux.NewRouter()
-	router.HandleFunc("/hello", handleRequest)
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(handleRequest)
 
-	router.ServeHTTP(rr, req)
+    handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
 
-	expected := "Hello World!"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
+    expected := "Welcome to my API!"
+    if rr.Body.String() != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+            rr.Body.String(), expected)
+    }
 }
