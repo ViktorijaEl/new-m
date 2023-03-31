57d68c702df78ae9a1fa1919ee97f5136a29c254
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-03-31 20:05:42 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handleRequest() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my API!")
+	})
+
+	http.ListenAndServe(":8080", router)
+}
 
 func main() {
-    fmt.Println("Hello, world!")
+	handleRequest()
 }
