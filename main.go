85978b0aff11bfee4677d7246e66793e4470ae97
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-03-31 20:16:36 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 
-	"github.com/gorilla/mux"
+    "github.com/gorilla/mux"
 )
 
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to my API!")
+}
+
 func main() {
-	router := mux.NewRouter()
+    router := mux.NewRouter()
+    router.HandleFunc("/", handleRequest)
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my API!")
-	})
-
-	http.ListenAndServe(":8080", router)
+    fmt.Println("Server listening on port 8080")
+    http.ListenAndServe(":8080", router)
 }
