86a0b4a75972417f95e1d0e5a8d11abd51bb42fd
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-04-05 15:37:37 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,53 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 
-    "github.com/gorilla/mux"
+	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/github"
+	"github.com/joho/godotenv"
 )
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to my API!")
-}
-
 func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/", handleRequest)
 
-    fmt.Println("Server listening on port 8080")
-    http.ListenAndServe(":8080", router)
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	appID, err := strconv.ParseInt(os.Getenv("APP_ID"), 10, 64)
+	installationID, err := strconv.ParseInt(os.Getenv("INSTALLATION_ID"), 10, 64)
+    privateKey := os.Getenv("PRIVATE_KEY")
+
+	// Shared transport to reuse TCP connections.
+	tr := http.DefaultTransport
+
+	// Wrap the shared transport for use with the app ID 1 authenticating with installation ID 99.
+	itr, err := ghinstallation.NewKeyFromFile(tr, appID, installationID, privateKey)
+	// itr, err := ghinstallation.NewAppsTransportKeyFromFile(tr, appID, privateKey)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Use installation transport with github.com/google/go-github
+	client := github.NewClient(&http.Client{Transport: itr})
+
+	repos, _, err := client.Apps.ListRepos(context.Background(), nil)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(repos[0])
+
+	// Print the name of each repository
+	for _, repo := range repos {
+		fmt.Println(*repo.Name)
+	}
 }
+
