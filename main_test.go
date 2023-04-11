1b11b8fd29929b4ded6d2ab639e01dfa00c76af7
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-04-11 17:01:10 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,93 +1,47 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"strconv"
 	"testing"
 
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/github"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
-	// Set up test environment variables
-	os.Setenv("APP_ID", "123")
-	os.Setenv("INSTALLATION_ID", "456")
-	os.Setenv("PRIVATE_KEY", "test-private-key")
+func TestListRepos(t *testing.T) {
 
-	// Create test client
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	appID := int64(12345)
+	installationID := int64(67890)
+	privateKey := os.Getenv("PRIVATE_KEY")
+
+	// Create a test server to mock the GitHub API responses
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-	})
-	testServer := httptest.NewServer(handler)
-
-	tr := &http.Transport{
-		Proxy: func(req *http.Request) (*url.URL, error) {
-			return url.Parse(testServer.URL)
-		},
-	}
+		w.Write([]byte(`[{"name":"test-repo"}]`))
+	}))
 	defer testServer.Close()
 
-	// Create test context and transport
-	ctx := context.Background()
-	itr, err := ghinstallation.New(tr, 123, 456, []byte("test-private-key"))
+	// Use the test server URL for the GitHub API endpoint
+	client := github.NewClient(&http.Client{})
+	client.BaseURL = testServer.URL + "/"
+
+	// Wrap the shared transport for use with the app ID 1 authenticating with installation ID 99.
+	itr, err := ghinstallation.New(http.DefaultTransport, appID, installationID, []byte(privateKey))
 	if err != nil {
-		t.Fatalf("Error creating installation transport: %v", err)
+		t.Fatal(err)
 	}
-	client := github.NewClient(&http.Client{Transport: itr})
+	client.Transport = itr
 
-	// Create test repositories
-	repos := []*github.Repository{
-		{Name: github.String("test-repo-1")},
-		{Name: github.String("test-repo-2")},
-		{Name: github.String("test-repo-3")},
-	}
-
-	// Set up test response
-	respBody := bytes.NewBuffer(nil)
-	for _, repo := range repos {
-		respBody.WriteString(*repo.Name + "\n")
-	}
-	resp := &http.Response{
-		StatusCode: http.StatusOK,
-		Body:       ioutil.NopCloser(respBody),
-	}
-
-	// Test main function
-	oldStdout := os.Stdout
-	defer func() { os.Stdout = oldStdout }()
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	main()
-
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-
-	if string(out) != "test-repo-1\ntest-repo-2\ntest-repo-3\n" {
-		t.Errorf("Unexpected output: %v", string(out))
-	}
-
-	// Test client method call
-	client.Transport = tr
-	client.BaseURL = testServer.URL
-	gotRepos, _, err := client.Apps.ListRepos(ctx, nil)
+	repos, _, err := client.Apps.ListRepos(context.Background(), nil)
 	if err != nil {
-		log.Fatalf("Error listing repositories: %v", err)
+		t.Fatal(err)
 	}
 
-	if len(gotRepos) != len(repos) {
-		t.Errorf("Expected %d repositories, but got %d", len(repos), len(gotRepos))
-	}
-	for i := range repos {
-		if *gotRepos[i].Name != *repos[i].Name {
-			t.Errorf("Expected repository name %q, but got %q", *repos[i].Name, *gotRepos[i].Name)
-		}
-	}
+	// Assert that the list of repositories contains the expected repository name
+	assert.Equal(t, "test-repo", *repos[0].Name)
 }
