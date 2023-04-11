164a0d3fe340d4f2e797edcbe2eed07547e8bab3
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-04-11 17:16:46 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,50 +1,44 @@
 package main
 
 import (
-	"context"
-	"net/http"
-	"net/http/httptest"
-	"os"
-	"testing"
+    "fmt"
+    "net/http"
+    "net/http/httptest"
+    "testing"
 
-	"github.com/bradleyfalzon/ghinstallation"
-	"github.com/google/go-github/github"
+    "github.com/google/go-github/v33/github"
 )
 
-func TestListRepos(t *testing.T) {
-	// Set up a test server to receive the HTTP requests from the client
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(`[{"name": "repo1"}, {"name": "repo2"}]`))
-	}))
-	defer testServer.Close()
+func TestGetRepositories(t *testing.T) {
+    // Create a test server with a mocked response
+    ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, `[{"name": "repo1"}, {"name": "repo2"}]`)
+    }))
+    defer ts.Close()
 
-	// Use the test server URL as the base URL for the client
-	baseURL := testServer.URL + "/"
+    // Set up a GitHub client that uses the test server as its base URL
+    client := github.NewClient(nil)
+    baseURL := ts.URL + "/"
+    u, err := url.Parse(baseURL)
+    if err != nil {
+        t.Fatalf("error parsing URL: %v", err)
+    }
+    client.BaseURL = u
 
-	// Set up a client with a transport that uses the test server URL
-	tr := http.DefaultTransport
-	itr, err := ghinstallation.New(tr, 1, 99, []byte(os.Getenv("PRIVATE_KEY")))
-	if err != nil {
-		t.Fatal(err)
-	}
-	client := github.NewClient(&http.Client{Transport: itr})
-	client.BaseURL = baseURL
+    // Call the function being tested
+    repos, err := GetRepositories(client)
+    if err != nil {
+        t.Fatalf("error getting repositories: %v", err)
+    }
 
-	// Call the ListRepos method on the client
-	repos, _, err := client.Apps.ListRepos(context.Background(), nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Verify that the returned repositories have the expected names
-	if len(repos) != 2 {
-		t.Fatalf("expected 2 repositories, got %d", len(repos))
-	}
-	if *repos[0].Name != "repo1" {
-		t.Errorf("expected first repository name to be 'repo1', got '%s'", *repos[0].Name)
-	}
-	if *repos[1].Name != "repo2" {
-		t.Errorf("expected second repository name to be 'repo2', got '%s'", *repos[1].Name)
-	}
+    // Verify the results
+    if len(repos) != 2 {
+        t.Fatalf("expected 2 repositories, got %d", len(repos))
+    }
+    if repos[0].Name != "repo1" {
+        t.Errorf("expected first repository name to be repo1, got %s", repos[0].Name)
+    }
+    if repos[1].Name != "repo2" {
+        t.Errorf("expected second repository name to be repo2, got %s", repos[1].Name)
+    }
 }
