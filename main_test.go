cadfeb2e0ba11055934322b5ce35314499322faf
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-04-11 17:06:05 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -4,40 +4,69 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strconv"
 	"testing"
 
+	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/github"
 )
 
-func TestListRepos(t *testing.T) {
-	// Create a test server with a mocked response
+func TestMain(m *testing.M) {
+	// Set up a test server to respond to API requests
 	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Check that the correct API endpoint is being called
-		if r.URL.Path != "/app/installations/123/repositories" {
-			t.Errorf("unexpected request path: %s", r.URL.Path)
-		}
-		// Return a JSON response with one repository
-		w.Write([]byte(`[{"id": 1, "name": "repo1"}]`))
+		w.Write([]byte(`[{"name": "repo1"}, {"name": "repo2"}]`))
 	}))
 	defer testServer.Close()
 
-	// Create a new GitHub client that uses the test server as the HTTP transport
-	client := github.NewClient(nil)
-	client.BaseURL = testServer.URL + "/"
+	// Override the GitHub API endpoint to use the test server
+	os.Setenv("GITHUB_API_URL", testServer.URL)
 
-	// Set the client to use the test server's transport
-	client.SetDo(func(req *http.Request) (*http.Response, error) {
-		req.URL.Scheme = testServer.URL
-		req.URL.Host = ""
-		return http.DefaultTransport.RoundTrip(req)
-	})
+	// Run the tests
+	exitVal := m.Run()
 
-	// Call the ListRepos function and check the result
+	os.Exit(exitVal)
+}
+
+func TestListRepos(t *testing.T) {
+	appID, err := strconv.ParseInt(os.Getenv("APP_ID"), 10, 64)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	installationID, err := strconv.ParseInt(os.Getenv("INSTALLATION_ID"), 10, 64)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	privateKey := os.Getenv("PRIVATE_KEY")
+
+	// Shared transport to reuse TCP connections.
+	tr := http.DefaultTransport
+
+	// Wrap the shared transport for use with the app ID 1 authenticating with installation ID 99.
+	itr, err := ghinstallation.New(tr, appID, installationID, []byte(privateKey))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Use installation transport with github.com/google/go-github
+	client := github.NewClient(&http.Client{Transport: itr})
+
 	repos, _, err := client.Apps.ListRepos(context.Background(), nil)
 	if err != nil {
-		t.Errorf("unexpected error: %v", err)
+		t.Fatal(err)
 	}
-	if len(repos) != 1 || *repos[0].Name != "repo1" {
-		t.Errorf("unexpected repos: %v", repos)
+
+	if len(repos) != 2 {
+		t.Fatalf("Expected 2 repos, but got %d", len(repos))
+	}
+
+	if *repos[0].Name != "repo1" {
+		t.Errorf("Expected first repo name to be repo1, but got %s", *repos[0].Name)
+	}
+
+	if *repos[1].Name != "repo2" {
+		t.Errorf("Expected second repo name to be repo2, but got %s", *repos[1].Name)
 	}
 }
