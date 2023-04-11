efb4668a64f81a3b6929416a4d63a57dad94dac1
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-04-11 17:28:58 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,42 +1,51 @@
-package main_test
+package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
-	"net/http/httptest"
 	"os"
-	"testing"
+	"strconv"
 
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/github"
 )
 
-func TestGetRepositories(t *testing.T) {
-	// Set up a test server
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`[]`))
-	}))
-	defer testServer.Close()
+func main() {
+	appID, err := strconv.ParseInt(os.Getenv("APP_ID"), 10, 64)
+	if err != nil {
+		log.Fatalf("error parsing APP_ID: %s", err)
+	}
 
-	// Use the test server's URL in place of the GitHub API's URL
-	baseURL := testServer.URL
+	installationID, err := strconv.ParseInt(os.Getenv("INSTALLATION_ID"), 10, 64)
+	if err != nil {
+		log.Fatalf("error parsing INSTALLATION_ID: %s", err)
+	}
 
-	// Set up a new http client with the test server's URL and authentication
+	privateKey := os.Getenv("PRIVATE_KEY")
+
+	// Shared transport to reuse TCP connections.
 	tr := http.DefaultTransport
-	itr, err := ghinstallation.New(tr, 1234, 5678, []byte("dummy key"))
-	if err != nil {
-		t.Errorf("Failed to create installation transport: %v", err)
-	}
-	client := github.NewClient(&http.Client{Transport: itr, BaseURL: baseURL})
 
-	// Call the function to get the repositories
-	repos, err := client.Repositories.List(nil)
+	// Wrap the shared transport for use with the app ID 1 authenticating with installation ID 99.
+	itr, err := ghinstallation.New(tr, appID, installationID, []byte(privateKey))
 	if err != nil {
-		t.Errorf("Failed to get repositories: %v", err)
+		log.Fatalf("error creating ghinstallation transport: %s", err)
 	}
 
-	// Ensure that the returned repository count is 0
-	if len(repos) != 0 {
-		t.Errorf("Expected 0 repositories, but got %d", len(repos))
+	// Use installation transport with github.com/google/go-github
+	client := github.NewClient(&http.Client{Transport: itr})
+
+	// List the repositories for the authenticated installation
+	ctx := context.Background()
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	if err != nil {
+		log.Fatalf("error listing repositories: %s", err)
+	}
+
+	// Print the name of each repository
+	for _, repo := range repos {
+		fmt.Println(*repo.Name)
 	}
 }
