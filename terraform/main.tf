7e6c0be2380441a8a6c50f093bbc7b645dc3d1b8
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-05-08 17:23:11 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,3 @@
-# provider "google" {
-#   project = "dev-infra-380007"
-#   region  = "eu"
-# }
-
-# resource "google_storage_bucket" "tf-bucket" {
-#   project       = "dev-infra-380007"
-#   name          = "buketdfndjkfj"
-#   location      = "eu"
-#   force_destroy = true
-#   public_access_prevention = "enforced"
-#   uniform_bucket_level_access = true
-#   storage_class = "Standard" 
-
-#   versioning {
-#     enabled = true
-#   }
-
-#   logging {
-#     log_bucket = "dev-github-terraform"
-#   }
-# }
 provider "aws" {
   region = var.region
   access_key = var.access_key
