82a80d624b0e88f8c7d4217183a3bcdf64c2d9b9
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-05-08 19:03:23 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,7 +71,13 @@ resource "aws_security_group" "example" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow DocumentDB traffic from all IP addresses"
   }
-  
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Deny RDP traffic from all IP addresses"
+  }  
   egress {
     from_port   = 0
     to_port     = 0
