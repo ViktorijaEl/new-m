c8c3bbb95992d9b8d62ea6b98c5a3cc2467c1b95
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-05-08 18:50:11 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,12 +69,15 @@ resource "aws_security_group" "example" {
     to_port     = 27017
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow DocumentDB traffic from all IP addresses"
     }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+    
   }
 }
 # Create the DocumentDB cluster
